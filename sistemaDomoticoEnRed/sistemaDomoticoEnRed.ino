a42b7d1b580def0149290326f840cd00b287da6f
Jorge Guani <jorge_guani@hotmail.com>
2018-12-10 18:22:32 -0600

    Se mejoró el código (funcionalidad total)

--- sistemaDomoticoEnRed/sistemaDomoticoEnRed.ino
+++ sistemaDomoticoEnRed/sistemaDomoticoEnRed.ino
@@ -1,34 +1,38 @@
+/*
+ * INSTITUTO TECNOLÓGICO DE LEÓN
+ * SISTEMA DOMÓTICO EN RED (WIFI)
+ * ELABORADO POR:
+  * JORGE ENRIQUE AGUADO GUANÍ
+  * NATALIA MÉNDEZ MARTÍNEZ
+*/
+
 #include <ESP8266WiFi.h>
 #include <DHT11.h>
 
+/* Cambie el ssid y contraseña por su red WiFi*/
 const char* ssid = "INFINITUM3021_2.4";
 const char* password = "9nNxwB07ba";
-//const char* ssid = "INFINITUM733AD1";
-//const char* password = "09A510608C";
 
-int foco = 13;     // GPIO13
-int ledRojo = 14;  // GPIO14
-int buzzer = 12;   // GPIO12
-int ledVerde = 5;  // GPIO5
-
-const int sensorPin = 2;
-
+/* Puerto 80 HTTP */
 WiFiServer server(80);
 
-//Variables para el sensor de luminosidad
-const long A = 1000;      //Resistencia en oscuridad en KΩ
-const int B = 15;         //Resistencia a la luz (10 Lux) en KΩ
-const int Rc = 10;        //Resistencia calibracion en KΩ
-const int PINLUZ = A0;    //Entrada analógica 0
-int valorAnalogicoLuz = 0;
-int valorLuminosidad = 0;
+/* Salidas digitales GPIO*/
+int foco = 13;     
+int ledRojo = 14;  
+int buzzer = 12;   
+const int in1Pin = 5;   //Entrada 2 del puente-H
+const int in2Pin = 16;  // Entrada 7 del puente-H
+const int sensorPin = 2;
 
-// Variables sensor de temperatura
-int pin = 4;
+/* Variables sensor de temperatura */
+int pin = 4;  //GPIO4
 DHT11 dht11(pin);
 
+/* Variables para el control de la casa */
 boolean focoPrendidoPorFaltaDeLuz = false;
 boolean alarmaPorIntruso = false;
+boolean puertaAbierta = false;
+
 
 void setup() {
   Serial.begin(115200);
@@ -37,15 +41,15 @@ void setup() {
   pinMode(foco, OUTPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(buzzer, OUTPUT);
-  pinMode(ledVerde, OUTPUT);
+  pinMode(in1Pin, OUTPUT); 
+  pinMode(in2Pin, OUTPUT);
   pinMode(sensorPin , INPUT);
   
   noTone(buzzer);
   digitalWrite(foco,LOW);
   digitalWrite(ledRojo,LOW);
-  digitalWrite(ledVerde,LOW);
 
-  // Connect to WiFi network
+/* Conecta a la red WiFi */
   Serial.println();
   Serial.println();
   Serial.print("Connecting to: ");
@@ -60,11 +64,11 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
-  // Start the server
+/* Inicia el servidor */
   server.begin();
   Serial.println("Server started");
 
-  // Print the IP address
+/* Imprime la IP del servidor */
   Serial.print("Use this URL to connect: ");
   Serial.print("http://");
   Serial.print(WiFi.localIP());
@@ -73,34 +77,33 @@ void setup() {
 }
 
 void loop() {  
-  // Luminosidad
-  // read the input on analog pin 0:
+/* ---Luminosidad (sensor 1)--- */
+  // Lee el valor de la salida analógica del dispositivo
   int sensorValue = analogRead(A0);
   
-  // Convert the analog reading (which goes from 0 - 1023) to a luminosidad (0 - 5V):
+  // Convierte el valor (0 - 1023) a luminosidad en voltaje (0 - 5V):
   float luminosidad = sensorValue * (5.0 / 1023.0) *200;
   
-  // print out the value you read:
+  // Imprime el valor que se lee de la fotoresistencia
   Serial.print("Valor luz: ");
   Serial.println(luminosidad);                         
-  
+
+  // Si la luminosidad es baja, enciende el foco de la casa (led amarillo)
   if(luminosidad < 10) {
-    //tone(foco,1000);
     focoPrendidoPorFaltaDeLuz = true;
     digitalWrite(foco, HIGH);
   }
 
   delay(200);
 
-  // Infrarrojo
-  // Sensor infrarrojo
+/* ---Sensor Infrarrojo (sensor 2)--- */
   int sensorInfrarrojo = 0;
   sensorInfrarrojo = digitalRead(sensorPin);  //lectura digital de pin del infrarrojo
 
   Serial.println(sensorInfrarrojo);
- 
+
+  // Si detecta algo, su señal es LOW (0) y enciende la alarma
   if (sensorInfrarrojo == LOW) {
-      //Serial.println("Detectado obstaculo");
       digitalWrite(ledRojo, HIGH);
       tone(buzzer,1000);
       alarmaPorIntruso = true;
@@ -108,57 +111,69 @@ void loop() {
     
   }
 
-  // Temperatura
+/* ---Temperatura (sensor 3)--- */
   int err;
   float temp, hum;
-  if((err = dht11.read(hum, temp)) == 0) { // Si devuelve 0 es que ha leido bien
-         Serial.print("Temperatura: ");
-         Serial.print(temp);
-         Serial.print(" Humedad: ");
-         Serial.print(hum);
-         Serial.println();
-      }
-   else {
-       Serial.println();
-       Serial.print("Error Num :");
-       Serial.print(err);
-       Serial.println();
-    }
+  if((err = dht11.read(hum, temp)) == 0) { // Si devuelve 0 es que ha leído bien
+    //Imprime el valor de la temperatura y humedad leídos
+    Serial.print("Temperatura: ");
+    Serial.print(temp);
+    Serial.print(" Humedad: ");
+    Serial.print(hum);
+    Serial.println();
+   } else {
+    Serial.println();
+    Serial.print("Error Num :");
+    Serial.print(err);
+    Serial.println();
+   }
   
   delay(1000);
+
+/* ---Uso del actuador---*/
+  //Si la temperatura es excesiva, se abre la ventanilla para permitir el paso a la ventilación
+  if(temp > 23 && puertaAbierta == false) {
+    // Abrir puerta    
+    digitalWrite(in1Pin,LOW); 
+    digitalWrite(in2Pin,HIGH);
+    delay(400);
+    // Parar
+    digitalWrite(in1Pin,LOW);
+    digitalWrite(in2Pin,LOW);
+    puertaAbierta = true;
+  }
   
+  delay(100);
+
+/* ---Cliente---*/
   WiFiClient client = server.available();
   if(!client) {
     return;
   }
 
-  // Wait until the client sends some data
+  // Espera hasta que el cliente envía algo
   Serial.println("new client");
   while(!client.available()); {
     delay(1);
   }
 
-  // Read the first line of the request
+  // Lee la primera línea de la solicitud
   String request = client.readStringUntil('\r');
   Serial.println(request);
   client.flush();
   
-  
 
-
-  // Match the request
-
+/* ---Coincidencias de la solicitud  del cliente--- */
   int value = LOW;
   int valueRed = LOW;
+  int valueMotor = LOW;
   
   if(request.indexOf("/LED=ON") != -1) {
     digitalWrite(foco, HIGH);
-    //tone(foco,1000);
     value = HIGH;
   }
   if(request.indexOf("/LED=OFF") != -1) {
     digitalWrite(foco, LOW);
-    //noTone(foco);
     value = LOW;
     focoPrendidoPorFaltaDeLuz = false;
   }
@@ -174,16 +189,38 @@ void loop() {
     alarmaPorIntruso = false;
   }
   if(request.indexOf("/REFRESH=ON") != -1) {
+    // Se usa para actualizar la página del servidor
+  }
+  if(request.indexOf("/DOOR=OPEN") != -1) {
+    // Abrir puerta
+    if(valueMotor == LOW) {      
+      digitalWrite(in1Pin,LOW); 
+      digitalWrite(in2Pin,HIGH);
+      delay(400);
+      // Parar
+      digitalWrite(in1Pin,LOW);
+      digitalWrite(in2Pin,LOW);
+      valueMotor = HIGH;
+      puertaAbierta = true;
+    }
     
   }
-  
-  
-/*--- Sección página web--- */
+  if(request.indexOf("/DOOR=CLOSE") != -1) {
+    // Cerrar puerta
+    digitalWrite(in1Pin,HIGH);
+    digitalWrite(in2Pin,LOW);
+    delay(400);
+    // Parar
+    digitalWrite(in1Pin,LOW);
+    digitalWrite(in2Pin,LOW);
+    valueMotor = LOW;
+    puertaAbierta = false;    
+  }
 
-  // Set foco according to the request
-  // digitalWrite(foco, value);
+/* --- Sección: página web--- */
+  // El servidor web responde de acuerdo a la solicitud del cliente
 
-  //Return the response
+  //Regresa la respuesta
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
   client.println("");
@@ -221,16 +258,16 @@ void loop() {
   client.println("<a href=\"/LEDROJO=ON\"\"><button>Prender alarma </button></a>");
   client.println("<a href=\"/LEDROJO=OFF\"\"><button>Apagar alarma </button></a><br /> <br />");
 
-  /*client.print("La puerta esta: ");
+  client.print("La puerta esta: ");
 
-  if(value == HIGH) {
-    client.print("Abierta");
+  if(valueMotor == HIGH || puertaAbierta == true) {
+      client.print("Abierta");   
   }else {
     client.print("Cerrada");    
   }
   client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>Abrir puerta </button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>Cerrar puerta </button></a><br />");*/
+  client.println("<a href=\"/DOOR=OPEN\"\"><button>Abrir puerta </button></a>");
+  client.println("<a href=\"/DOOR=CLOSE\"\"><button>Cerrar puerta </button></a><br />");
   client.println("</div>");
   client.println("</html>");
   
